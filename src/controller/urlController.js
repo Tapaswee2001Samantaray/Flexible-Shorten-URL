c4f035d3a7164e737f0215b64a87d98afe56c038
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-01-17 12:06:52 +0530

    phase I completed

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -2,44 +2,45 @@ const checkValidUrl = require("valid-url");
 const ShortId = require("shortid");
 const urlModel = require("../model/urlModel");
 
+
+//==============router handler for short url===============
 const createShortUrl = async function (req, res) {
     try {
         let data = req.body;
         let longUrl = data.longUrl;
 
-        if (Object.keys(data).length != 0) {
-
-            if (!longUrl || longUrl == "") {
-                return res.status(400).send({ status: false, message: "Long URL is required and Long URL can not be empty." });
-            }
-
-            if (typeof longUrl != "string") {
-                return res.status(400).send({ status: false, message: "Long url must be String." });
-            }
-
-            if (!checkValidUrl.isWebUri(longUrl.trim())) {
-                return res.status(400).send({ status: false, message: "Please Enter a valid URL." });
-            }
-
-            const findUrlDetails = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
-            if (findUrlDetails) {
-                return res.status(200).send({ status: true, data: findUrlDetails });
-            }
-
-            let uniqueUrlCode = ShortId.generate();
-            data.urlCode = uniqueUrlCode;
-
-            let shortUrl = " http://localhost:3000/" + uniqueUrlCode;
-            data.shortUrl = shortUrl;
-
-            const createUrlData = await urlModel.create(data);
-
-            const finalResult = await urlModel.findById(createUrlData._id).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
-
-            res.status(201).send({ status: true, data: finalResult });
-        } else {
-            return res.status(400).send({ status: false, message: "Invalid request." });
+        //======long URL validation=====
+        if (!longUrl || longUrl == "") {
+            return res.status(400).send({ status: false, message: "Long URL is required and Long URL can not be empty." });
         }
+
+        if (typeof longUrl != "string") {
+            return res.status(400).send({ status: false, message: "Long url must be String." });
+        }
+
+        if (!checkValidUrl.isWebUri(longUrl.trim())) {
+            return res.status(400).send({ status: false, message: "Please Enter a valid URL." });
+        }
+
+        //=====check if long URL exists and show its details======
+        const findUrlDetails = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
+        if (findUrlDetails) {
+            return res.status(200).send({ status: true, data: findUrlDetails });
+        }
+
+        //====if long url is unique then generate URL code and short URL=====
+        let uniqueUrlCode = ShortId.generate();
+        data.urlCode = uniqueUrlCode;
+
+        let shortUrl = "http://localhost:3000/" + uniqueUrlCode;
+        data.shortUrl = shortUrl.toLowerCase();
+
+        //====here we are creating tha data=====
+        const createUrlData = await urlModel.create(data);
+
+        const finalResult = await urlModel.findById(createUrlData._id).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
+
+        res.status(201).send({ status: true, data: finalResult });
     } catch (err) {
         return res.status(500).send({ status: false, message: err.message });
     }
@@ -47,28 +48,29 @@ const createShortUrl = async function (req, res) {
 }
 
 
-const getUrl = async function(req , res){
-    try{
+//==============router handler for get url===============
+const getUrl = async function (req, res) {
+    try {
         let data = req.params;
         let urlCode = data.urlCode;
-        if(Object.keys(data).length != 0){
-            
-            if(!urlCode){
-                return res.status(400).send({status:false , message:"URL Code is required."});
-            }
-
-            let checkUrlCode = await urlModel.findOne({urlCode:urlCode});
-            if(!checkUrlCode){
-                return res.status(404).send({status:false , message:"No URL found with this URL code."});
-            }
-
-            res.status(302).redirect(checkUrlCode.longUrl);
-        }else{
-            return res.status(400).send({status:false , message:"Invalid request.Please provide valid information."});
+        
+        //=====validation for URL code=======
+        if (!urlCode) {
+            return res.status(400).send({ status: false, message: "URL Code is required." });
         }
-    }catch(err){
-        return res.status(500).send({status:false , message:err.message});
+
+        //======fetch the details based on the URL code======
+        let checkUrlCode = await urlModel.findOne({ urlCode: urlCode });
+        if (!checkUrlCode) {
+            return res.status(404).send({ status: false, message: "No URL found with this URL code." });
+        }
+
+        //=====redirecting to the Long URL based on the URL code=====
+        res.status(302).redirect(checkUrlCode.longUrl);
+        
+    } catch (err) {
+        return res.status(500).send({ status: false, message: err.message });
     }
 }
 
-module.exports = { createShortUrl , getUrl};
+module.exports = { createShortUrl, getUrl };
