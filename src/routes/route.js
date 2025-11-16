ef16363a01a8c2f41953511461ef03c4e39191b9
Tapaswee Samantaray <119597242+Tapaswee2001Samantaray@users.noreply.github.com>
2025-11-16 12:41:04 +0530

    Update route.js || added test route

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const urlController = require("../controller/urlController");
 
+router.get("/", function (req, res) {
+    res.status(200).send("Server is up and running.");
+});
+
 router.post("/url/shorten" , urlController.createShortUrl);
 router.get("/:urlCode" , urlController.getUrl);
 
